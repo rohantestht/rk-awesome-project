d294deb29e0214372a2474aee9869f3004932a8e
Rohan Kadam <115790284+rohantestht@users.noreply.github.com>
2023-01-24 11:54:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
         console.log('New connection')
-        res.end('Hello Darshan Sir')
+        res.end('Hello 123456 Sir')
 })
 const PORT =  process.env.PORT || 8080
 server.listen(PORT, () => console.log('listening'))
